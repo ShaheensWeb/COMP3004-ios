1ab7c72dc73985de044c84f94e2ce6d6ac99e9ad
James McConnell <james.mcconnell@Jamess-MacBook-Pro.local>
2017-11-26 17:07:13 -0500

    ui label broken

--- safeStart/safeStart/ViewController.swift
+++ safeStart/safeStart/ViewController.swift
@@ -9,6 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var seconds = 60
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    @IBAction func startButton(_ sender: Any) {
+    }
+    @IBAction func stopButton(_ sender: Any) {
+    }
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+        isTimerRunning = true
+    }
+    @objc func updateTimer() {
+        if seconds < 1 {
+            timer.invalidate()
+            //Send alert to indicate time's up.
+        } else {
+            seconds -= 1
+            timerLabel.text = timeString(time: TimeInterval(seconds))
+            timerLabel.text = String(seconds)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +42,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
